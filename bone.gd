148a859b61a978b345244ba0a7d99c55a8101c74
Soup-Wizard <cosmic8gumbo@gmail.com>
2024-09-23 00:30:10 -0400

    It's done

--- bone.gd
+++ bone.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 var collected = false
+@onready var boneNear = $Sprite2D/BoneNear
+@onready var boneGet = $Sprite2D/BoneGet
+@onready var boneDone = $Sprite2D/BoneDone
 
 func _ready():
+	#boneNear.play()
 	var tween = create_tween().set_loops()
 	tween.tween_property($Sprite2D, "position", Vector2($Sprite2D.position.x, $Sprite2D.position.y - 3), 0.7)
 	tween.tween_property($Sprite2D, "position", Vector2($Sprite2D.position.x, $Sprite2D.position.y), 0.7)
@@ -10,7 +14,18 @@ func _ready():
 func _on_body_entered(body):
 	if !collected:
 		collected = true
+		visible = false
+		boneNear.stop()
 		body.bones += 1
 		if body.bones == 5 || body.bones == 10 || body.bones == 15:
 			body.lives += 1
-		queue_free()
+		if body.bones < 15:
+			boneGet.play()
+		else:
+			boneDone.play()
+
+func _on_bone_get_finished():
+	queue_free()
+
+func _on_bone_done_finished():
+	queue_free()
